21026a1f2beb9f7a16774037f41fb1e47e352778
shivansh moore <shivanshmoore05@gmail.com>
2024-09-10 08:32:36 +0100

    Work on all desktops now

--- vite-project/src/components/User_messages.jsx
+++ vite-project/src/components/User_messages.jsx
@@ -1,9 +1,9 @@
-const User_messages = (props) => {
+const User_messages = () => {
   return (
     <div className="flex justify-end">
       <div className="pb-[10px] pr-[10px]">
         <div className="flex h-[40px] flex-col justify-center rounded-xl bg-ikea-grey pl-[10px] pr-[10px]">
-          <div>{props.message}</div>
+          <div>{}</div>
         </div>
       </div>
     </div>
--- vite-project/src/components/Widget.jsx
+++ vite-project/src/components/Widget.jsx
@@ -6,14 +6,14 @@ import { useEffect, useState } from "react";
 
 const Widget = () => {
   return (
-    <div className="fixed bottom-[20px] right-[20px] flex h-[80vh] w-[30vw] flex-col overflow-auto rounded-xl border-2 border-ikea-yellow pb-[60px]">
+    <div className="fixed bottom-[20px] right-[20px] flex h-[80vh] max-h-[700px] w-[30vw] max-w-[500px] flex-col overflow-auto rounded-xl border-2 border-ikea-yellow pb-[60px]">
       <div className="flex-1 overflow-auto">
         <Logo />
-        <User_messages message={userMessage}/>
+        <User_messages />
         <Bot_messages />
       </div>
       <div className="mt-1">
-        <Form/>
+        <Form />
       </div>
     </div>
   );
